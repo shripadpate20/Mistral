e4a83b6838edc88b967c8210e7000902e767338c
shripadpate20 <shripadpate20@gmail.com>
2026-05-17 18:52:10 +0530

    Updating rms_norm with a cleaner RMSNorm implementation

--- model/rms_norm.py
+++ model/rms_norm.py
@@ -6,9 +6,9 @@ class RMSNorm(nn.Module):
     def __init__(self, d_model: int, eps: float = 1e-6):
         super().__init__()
         self.eps = eps # add small value to prevent division by zero
-        self.weight = nn.Parameter(torch.ones(d_model)) # add leqrnable weight parameter
+        self.weight = nn.Parameter(torch.ones(d_model)) # add learnable weight parameter
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        rms = torch.sqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+        rms = torch.sqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)  
         return x / rms * self.weight 
     
